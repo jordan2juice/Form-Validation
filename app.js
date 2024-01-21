84c4040e07495463c556d4fd6747e480e888e3d1
jordan toujouse <jtoujo1@gmail.com>
2024-01-20 19:09:29 -0600

    added verification to java script

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const form = document.querySelector("#myform");
+form.addEventListener("submit", (e) => {
+  if (form.username.value < 5) {
+    e.preventDefault();
+    alert("Your username is have more than 5 charectors!");
+  }
+  if (form.age.value < 18 || form.age.value > 65 || form.age.value == "") {
+    e.preventDefault();
+    alert("Your age is not valid!");
+  }
+  const emailregix =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+});
